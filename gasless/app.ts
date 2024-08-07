f3a22ee9f5fa888b461bffa8ad4edab4c5cd050a
dipanshuhappy <dipanshuhappy@gmail.com>
2024-08-08 02:47:40 +0530

    jj

--- gasless/app.ts
+++ gasless/app.ts
@@ -1,8 +1,42 @@
 import express from "express";
+import {z} from "zod"
+import { sendRawTransaction, } from "./wallet";
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.use(express.json());
+const TransactionSchema = z.object({
+   to: z.string(),
+   data: z.string(),
+   value: z.number(),
+   chainId: z.union([z.literal(1116), z.literal(137)])
+})
+
+const ENV = z.object({
+  KEY: z.string(),
+  COREDAO_RPC:z.string(),
+  POLYGON_RPC:z.string(),
+  PORT:z.string().optional()
+})
+console.log("hiiii")
+app.post("/transaction", (req, res) => {
+  const unparasedBody = req.body;
+  console.log(unparasedBody)
+  const env = ENV.parse(process.env)
+  const parsedTransaction = TransactionSchema.parse(unparasedBody)
+
+  const receipt = sendRawTransaction({
+    key: env.KEY as `0x${string}`,
+    rpc: parsedTransaction.chainId === 1116 ? env.COREDAO_RPC : env.POLYGON_RPC,
+    to: parsedTransaction.to as `0x${string}`,
+    value: parsedTransaction.value.toString(),
+    data: parsedTransaction.data as `0x${string}`,
+    chainId: parsedTransaction.chainId
+  })
+
+  return res.status(200).json(receipt);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
