86b83e3f42c1c46d424120aea264042f5720cbf0
dipanshuhappy <dipanshuhappy@gmail.com>
2024-08-07 11:51:23 +0530

    chore: Update globe configuration and Tailwind CSS plugin

--- commeme-frontend/src/components/ui/hero-highlight.tsx
+++ commeme-frontend/src/components/ui/hero-highlight.tsx
@@ -29,12 +29,12 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "relative h-[40rem] flex items-center bg-white dark:bg-black justify-center w-full group",
+        "relative h-[40rem] flex items-center bg-transparent dark:bg-black justify-center w-full group",
         containerClassName
       )}
       onMouseMove={handleMouseMove}
     >
-      <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none" />
+      <div className="absolute inset-0 bg-dot-thick-transparent-300 dark:bg-dot-thick-gray-800  pointer-events-none" />
       <motion.div
         className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
         style={{
@@ -86,7 +86,7 @@ export const Highlight = ({
         display: "inline",
       }}
       className={cn(
-        `relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r from-indigo-300 to-purple-300 dark:from-indigo-500 dark:to-purple-500`,
+        `relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r from-orange-500 to-orange-300 dark:from-orange-500 dark:to-orange-500`,
         className
       )}
     >
--- commeme-frontend/tailwind.config.js
+++ commeme-frontend/tailwind.config.js
@@ -3,6 +3,8 @@
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
+
+const svgToDataUri = require("mini-svg-data-uri");
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -77,7 +79,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"),addVariablesForColors],
+  plugins: [require("tailwindcss-animate"),addVariablesForColors,matchUtils],
 }
 
  
@@ -89,4 +91,17 @@ function addVariablesForColors({ addBase, theme }) {
   addBase({
     ":root": newVars,
   });
+}
+
+function matchUtils({ matchUtilities, theme }) {
+  matchUtilities(
+    {
+      "bg-dot-thick": (value) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+        )}")`,
+      }),
+    },
+    { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+  );
 }
