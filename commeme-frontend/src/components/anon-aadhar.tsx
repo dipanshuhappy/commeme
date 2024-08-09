604e30cc1b88f57e9fbbc9e9a2b7badd4fe6a0b2
dipanshuhappy <dipanshuhappy@gmail.com>
2024-08-09 08:57:14 +0530

    chore: Update addresses-data.ts with legacy address for gasless transaction

--- commeme-frontend/src/components/anon-aadhar.tsx
+++ commeme-frontend/src/components/anon-aadhar.tsx
@@ -1,10 +1,9 @@
 import {
-    LogInWithAnonAadhaar,
-    useAnonAadhaar,
-    AnonAadhaarProof,
-    useProver
-  } from "@anon-aadhaar/react";
-  import { useEffect, useState } from "react";
+  LogInWithAnonAadhaar,
+  useAnonAadhaar,
+  useProver
+} from "@anon-aadhaar/react";
+import { useEffect, useState } from "react";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { useChains, useWalletClient } from "wagmi";
@@ -14,65 +13,91 @@ import { CONSTANT_ADDRESSES, LEGACY_ABI } from "@/data/addresses-data";
 import { toast } from "sonner";
 import { SupportChainId } from "@/hooks/use-query-commemes";
 import { TransactionToast } from "./ui/transaction-toast";
-  
-  export default function Anon() {
-    const [anonAadhaar] = useAnonAadhaar();
-    const [, latestProof] = useProver();
 
-    const nullifierSeedString = "19566981402436238301701121519446139147227";
-    const nullifierBigInt = BigInt(nullifierSeedString);
-  
-    useEffect(() => {
-      console.log("Anon Aadhaar status: ", anonAadhaar.status);
-    }, [anonAadhaar]);
+export default function Anon() {
+  const [anonAadhaar] = useAnonAadhaar();
+  const [, latestProof] = useProver();
+  const nullifierSeedString = "19566981402436238301701121519446139147227";
+  const nullifierBigInt = BigInt(nullifierSeedString);
 
-    const chains = useChains();
-    const [selectedChain, setSelectedChain] = useState(1116)
-    const chainsIds = chains.map((chain) => chain.id)
-    const {data:wallet} = useWalletClient()
+  useEffect(() => {
+    console.log("Anon Aadhaar status: ", anonAadhaar.status);
+  }, [anonAadhaar]);
 
-    
-  
-    return (
-      <div className="w-full grid place-items-center">
-      <div>
-        <Label className="my-4">UBI FOR INDIAN CITIZEN POWERED BY COMMEME</Label>
-        <h3>Login with aadhar to receive UBI(Universal Basic Income)</h3>
-        <LogInWithAnonAadhaar  nullifierSeed={nullifierBigInt} />
-        
-      </div>
-      <div >
-        {/* Render the proof if generated and valid */}
-        {anonAadhaar?.status === "logged-in" && (
-          <>
-            <p>✅ Proof is valid</p>
-            {latestProof && (
-              <><Label htmlFor="chain">Select Which Blockchain (It's gasless to receive)</Label><RadioGroup defaultValue={selectedChain.toString()} onValueChange={e => setSelectedChain(parseFloat(e))}>
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value={`${chainsIds[0]}`} id="option-one" />
-                    <Label htmlFor="option-one">CoreDao</Label>
-                  </div>
-                  <div className="flex items-center space-x-2">
-                    <RadioGroupItem value={`${chainsIds[1]}`} id="option-two" />
-                    <Label htmlFor="option-two">Polygon Mainnet</Label>
-                  </div>
-                </RadioGroup><Button onClick={async () => {
-                    if(!wallet){
-                      toast.error("Please connect your wallet")
-                      throw new Error("Please connect your wallet")
+  const chains = useChains();
+  const [selectedChain, setSelectedChain] = useState(1116);
+  const chainsIds = chains.map((chain) => chain.id);
+  const { data: wallet } = useWalletClient();
 
-                    }
-                   const rawData = encodeFunctionData({
-                    abi: LEGACY_ABI,
-                    functionName: "getBack",
-                    args: [
-                      wallet.account.address
-                    ],
-                  });
-                  const addresses = CONSTANT_ADDRESSES[selectedChain as SupportChainId];
-                  console.log(rawData, "raw data");
-                  console.log(`${import.meta.env.VITE_RELAY}/transaction`);
-                  const res = await fetch(`${import.meta.env.VITE_RELAY}/transaction`, {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 py-12 px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8 bg-white shadow-lg rounded-lg p-8">
+        <div className="text-center">
+          <Label className="text-2xl font-semibold text-gray-800 mb-4">
+            UBI for Indian Citizens Powered by Commeme
+          </Label>
+          <p className="text-gray-600">
+            Login with Aadhaar to receive UBI (Universal Basic Income)
+          </p>
+        </div>
+        <div className="mt-6">
+          <LogInWithAnonAadhaar nullifierSeed={nullifierBigInt} />
+        </div>
+
+        {anonAadhaar?.status === "logged-in" && latestProof && (
+          <div className="mt-6 space-y-4">
+            <p className="text-green-600 font-medium">✅ Proof is valid</p>
+
+            <div className="space-y-2">
+              <Label className="font-medium text-gray-700">
+                Select Blockchain (It's gasless to receive)
+              </Label>
+              <RadioGroup
+                className="space-y-2"
+                defaultValue={selectedChain.toString()}
+                onValueChange={(e) => setSelectedChain(parseFloat(e))}
+              >
+                <div className="flex items-center">
+                  <RadioGroupItem value={`${chainsIds[0]}`} id="option-one" />
+                  <Label
+                    htmlFor="option-one"
+                    className="ml-2 text-gray-700 font-medium"
+                  >
+                    CoreDao
+                  </Label>
+                </div>
+                <div className="flex items-center">
+                  <RadioGroupItem value={`${chainsIds[1]}`} id="option-two" />
+                  <Label
+                    htmlFor="option-two"
+                    className="ml-2 text-gray-700 font-medium"
+                  >
+                    Polygon Mainnet
+                  </Label>
+                </div>
+              </RadioGroup>
+            </div>
+
+            <Button
+              className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-lg transition-colors"
+              onClick={async () => {
+                if (!wallet) {
+                  toast.error("Please connect your wallet");
+                  throw new Error("Please connect your wallet");
+                }
+                const rawData = encodeFunctionData({
+                  abi: LEGACY_ABI,
+                  functionName: "getBack",
+                  args: [wallet.account.address],
+                });
+                const addresses = CONSTANT_ADDRESSES[
+                  selectedChain as SupportChainId
+                ];
+                console.log(rawData, "raw data");
+                console.log(`${import.meta.env.VITE_RELAY}/transaction`);
+                const res = await fetch(
+                  `${import.meta.env.VITE_RELAY}/transaction`,
+                  {
                     body: JSON.stringify({
                       to: addresses.legacyAddress,
                       data: rawData,
@@ -83,21 +108,26 @@ import { TransactionToast } from "./ui/transaction-toast";
                     headers: {
                       "Content-Type": "application/json",
                     },
-                  });
-                  console.log({ res });
-                  const responseJson = (await res.json()) as unknown as { hash: string };
-                  toast.success(
-                    <TransactionToast
-                      hash={responseJson.hash}
-                      title="Ubi Received Deployed"
-                      scanner={`${addresses.scanner}/tx/`}
-                    />
-                  );
-                 } }>Receive</Button></>
-              )}
-          </>
-          )}
+                  }
+                );
+                console.log({ res });
+                const responseJson = (await res.json()) as unknown as {
+                  hash: string;
+                };
+                toast.success(
+                  <TransactionToast
+                    hash={responseJson.hash}
+                    title="UBI Received"
+                    scanner={`${addresses.scanner}/tx/`}
+                  />
+                );
+              }}
+            >
+              Receive
+            </Button>
+          </div>
+        )}
       </div>
-      </div>
-    );
-  }
+    </div>
+  );
+}
