0a489c6aea34f6652426d859c60fee2a920c0f64
Nithin-Varma <mnithin1422@gmail.com>
2024-08-09 07:34:08 +0530

    on chain verification support

--- contracts/contracts/core/anon.sol
+++ contracts/contracts/core/anon.sol
@@ -2,15 +2,20 @@
 pragma solidity ^0.8.19;
 
 import "@anon-aadhaar/contracts/interfaces/IAnonAadhaar.sol";
+import "@anon-aadhaar/contracts/interfaces/IAnonAadhaarGroth16Verifier.sol";
 
 contract AnonAadhaarReward {
-    address public anonAadhaarVerifierAddr;
+    address public verifier;
     uint256 public rewardAmount; 
+    uint256 public immutable storedPublicKeyHash;
+    address[] public totalUsers;
+    mapping(address => bool) public isUserVerified;
 
-    event UserVerifiedAndRewarded(address user, uint256 amount);
+    event ProofVerified(address user, bool result);
 
-    constructor(address _verifierAddr, uint256 _rewardAmount) {
-        anonAadhaarVerifierAddr = _verifierAddr;
+    constructor(address _verifierAddr, uint256 _rewardAmount, uint256 _pubkeyHash) {
+        verifier = _verifierAddr;
+        storedPublicKeyHash = _pubkeyHash;
         rewardAmount = _rewardAmount;
     }
 
@@ -21,34 +26,51 @@ contract AnonAadhaarReward {
     /// @param signal: Signal used while generating the proof, should be equal to msg.sender.
     /// @param revealArray: Array of the values used as input for the proof generation.
     /// @param groth16Proof: SNARK Groth16 proof.
-    function verifyAndReward(
+    function verifyAnonAadhaarProof(
         uint nullifierSeed,
         uint nullifier,
         uint timestamp,
         uint signal,
-        uint[4] memory revealArray,
-        uint[8] memory groth16Proof
-    ) public {
-        require(
-            IAnonAadhaar(anonAadhaarVerifierAddr).verifyAnonAadhaarProof(
-                nullifierSeed,
-                nullifier,
-                timestamp,
-                signal,
-                revealArray,
-                groth16Proof
-            ) == true,
-            '[AnonAadhaarReward]: Proof is not valid.'
-        );
-
-        payable(msg.sender).transfer(rewardAmount);
-
-        emit UserVerifiedAndRewarded(msg.sender, rewardAmount);
+        uint[4] calldata revealArray,
+        uint[8] calldata groth16Proof,
+        address sender
+    ) public returns (bool) {
+        if(isUserVerified[sender]) revert("UAE"); // UAE - User Already Exists
+        uint signalHash = _hash(signal);
+        bool result = IAnonAadhaarGroth16Verifier(verifier).verifyProof(
+                [groth16Proof[0], groth16Proof[1]],
+                [
+                    [groth16Proof[2], groth16Proof[3]],
+                    [groth16Proof[4], groth16Proof[5]]
+                ],
+                [groth16Proof[6], groth16Proof[7]],
+                [
+                    storedPublicKeyHash,
+                    nullifier,
+                    timestamp,
+                    // revealAgeAbove18
+                    revealArray[0],
+                    // revealGender
+                    revealArray[1],
+                    // revealPincode
+                    revealArray[2],
+                    // revealState
+                    revealArray[3],
+                    nullifierSeed,
+                    signalHash
+                ]
+            );
+        if(!result) revert("PNV,VNS"); // PNV,VNS - Proof Not Valid, Verification Not Successful
+        totalUsers.push(sender);
+        isUserVerified[sender] = true;
+        return result;
     }
 
-    function setRewardAmount(uint256 _rewardAmount) public {
-        rewardAmount = _rewardAmount;
+    function _hash(uint256 message) private pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(message))) >> 3;
     }
 
-    receive() external payable {}
+    function retrieveUsers() view public returns(address[] memory) {
+        return totalUsers;
+    }
 }
